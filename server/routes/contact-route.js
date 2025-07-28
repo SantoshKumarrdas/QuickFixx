7739dd5829c2c5a4872feaa6ae3eabf89907a8c4
Santosh Kumar <163877432+SantoshKumarrdas@users.noreply.github.com>
2025-07-28 18:42:37 +0530

    Update contact-route.js

--- server/routes/contact-route.js
+++ server/routes/contact-route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const contactForm = require("../controllers/contact-controller");
+const contactForm = require("../controllers/Contact-controller");
 
 router.route("/contact").post(contactForm);
 
